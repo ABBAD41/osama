5db2796dfacc49c786733cba570df0d822e26315
usamau32 <56486086+usamau32@users.noreply.github.com>
2019-11-13 17:00:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("642304835135209503")
 setInterval(function() {
-channel.send(`osama osama osama osama osama osama بحبكم بحبكم بحبكم osama كيفكم هلا هلا لاه  @يا اسامه @يا اسامه @@ هلا هل بحبكما هلا هلا هلا السلام عليكم كيفكم كيفكم osama osama osama`);
+channel.send(`osama osama osama osama osama #daily osama بحبكم بحبكم بحبكم osama كيفكم هلا هلا لاه  @يا اسامه @يا اسامه @@ هلا هل بحبكما هلا هلا هلا السلام عليكم كيفكم كيفكم osama osama osama`);
 }, 30)
 })
 
