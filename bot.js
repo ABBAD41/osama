0f071eb4d98063eefd0a579c77f2c02cc366335f
usamau32 <56486086+usamau32@users.noreply.github.com>
2019-11-13 16:27:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get(" 642304835135209503")
+let channel =     client.channels.get("642304835135209503")
 setInterval(function() {
 channel.send(`osama osama osama osama osama osama`);
 }, 30)
